3eb4204ae2474250d4515187afd4519cdc900480
RinatAvital <95857579+RinatAvital@users.noreply.github.com>
2021-12-13 14:07:59 +0200

    שינוי DAL

---
+++ server/Gragh/DAL/GraghEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DAL
+{
+    internal class GraghEntity
+    {
+        internal object Entry<T>(T entity) where T : class
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
