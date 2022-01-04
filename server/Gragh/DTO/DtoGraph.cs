9d8f069ac58fc6a523f0c20e44e94315a7bbd93a
RinatAvital <95857579+RinatAvital@users.noreply.github.com>
2022-01-04 15:40:52 +0200

    client

---
+++ server/Gragh/DTO/DtoGraph.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using Models;
+
+namespace DTO
+{
+    class DtoGraph
+    {
+        public long graphCode { get; set; }
+        public string graphString { get; set; }
+        public long userCode { get; set; }
+        public DateTime creationDate { get; set; }
+
+        public DtoGraph(Graphs g)
+        {
+            graphCode = (long)g.graphCode;
+            graphString = g.graphString;
+            userCode = (long)g.userCode;
+            creationDate = (DateTime)g.creationDate;
+        }
+        public static List<DtoGraph> DTOtoList(List<Graphs> list)
+        {
+            List<DtoGraph> DTOlist = new List<DtoGraph>();
+            foreach (var u in list)
+            {
+                DTOlist.Add(new DtoGraph(u));
+            }
+            return DTOlist;
+        }
+    }
+}
---
+++ server/Gragh/DTO/DtoUser.cs
@@ -0,0 +1,40 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace DTO
+{
+    class DtoUser
+    {
+        public long Code { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string userName { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+
+        public DtoUser(Users u)
+        {
+            Code = u.Code;
+            firstName = u.firstName;
+            lastName = u.lastName;
+            userName = u.userName;
+            email = u.email;
+            password = u.password;
+
+        } 
+        public static List<DtoUser> DTOtoList(List<Users> list)
+        {
+            List<DtoUser> DTOlist = new List<DtoUser>();
+            foreach (var u in list)
+            {
+                DTOlist.Add(new DtoUser(u));
+            }
+            return DTOlist;
+        }
+    }
+}
